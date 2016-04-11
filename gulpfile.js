1709cb2c0c7102e0db81bdf34734fd35ccec8372
Joseph Mulvaney <me@mycro.be>
2016-04-11 13:12:55 -0400

    replace unsupported gulp-browserify with straight browserify. compile the worker js in postinstall script for #1

--- gulpfile.js
+++ gulpfile.js
@@ -1,69 +1,82 @@
 var gulp = require('gulp');
-var gconcat = require('gulp-concat');
 var closure = require('gulp-closure-compiler');
-var browserify = require('gulp-browserify');
+var browserify = require('browserify');
 var rename = require('gulp-rename');
+var buffer = require('vinyl-buffer');
+var source = require('vinyl-source-stream');
 // var utils = require('gulp-util');
 // var gif = require('gulp-if');
 
 /* Want to gulp-if conditional compile everything, but currently doesn't seem to work */
 
-gulp.task('build-worker', function() {
-  gulp.src('js/fetchworker.js')
-  .pipe(browserify({
+gulp.task('build-worker', function () {
+  browserify({
+    entries: 'js/fetchworker.js',
     debug: true,
     nobuiltins: true
-  }))
-  .pipe(rename('worker-all.js'))
-  .pipe(gulp.dest('build/'));
+  }).bundle()
+    .pipe(source('js/fetchworker.js'))
+    .pipe(buffer())
+    .pipe(rename('worker-all.js'))
+    .pipe(gulp.dest('build/'));
 });
 
 
-gulp.task('build-main', function() {
-  gulp.src('js/exports.js')
-  .pipe(browserify({
+gulp.task('build-main', function () {
+  browserify({
+    entries: 'js/exports.js',
     debug: true,
     nobuiltins: true
-  }))
-  .pipe(rename('dalliance-all.js'))
-  .pipe(gulp.dest('build/'));
+  }).bundle()
+    .pipe(source('js/exports.js'))
+    .pipe(buffer())
+    .pipe(rename('dalliance-all.js'))
+    .pipe(gulp.dest('build/'));
 });
 
-gulp.task('compile-worker', function() {
-  gulp.src('js/fetchworker.js')
-  .pipe(browserify({
+gulp.task('compile-worker', function () {
+  browserify({
+    entries: 'js/fetchworker.js',
     debug: true,
     nobuiltins: true
-  }))
-  .pipe(rename('worker-all.js'))
-  .pipe(gulp.dest('tmp/'))
-  // .pipe(gif(!isDev, closure()))   // Doesn't work
-  .pipe(closure({compilerPath: 'node_modules/closure-compiler/lib/vendor/compiler.jar', 
-                 fileName: 'worker-all.js',
-                 compilerFlags: {
-                    language_in: 'ECMASCRIPT5'
-                 }}))
-  .pipe(gulp.dest('build/'));
+  }).bundle()
+    .pipe(source('js/fetchworker.js'))
+    .pipe(buffer())
+    .pipe(rename('worker-all.js'))
+    .pipe(gulp.dest('tmp/'))
+    // .pipe(gif(!isDev, closure()))   // Doesn't work
+    .pipe(closure({
+      compilerPath: 'node_modules/google-closure-compiler/compiler.jar',
+      fileName: 'worker-all.js',
+      compilerFlags: {
+        language_in: 'ECMASCRIPT5'
+      }
+    }))
+    .pipe(gulp.dest('build/'));
 });
 
-gulp.task('compile-main', function() {
-  gulp.src('js/exports.js')
-  .pipe(browserify({
+gulp.task('compile-main', function () {
+  browserify({
+    entries: 'js/exports.js',
     debug: true,
     nobuiltins: true
-  }))
-  .pipe(rename('dalliance-all.js'))
-  .pipe(gulp.dest('tmp/'))
-  // .pipe(gif(!isDev, closure()))   // Doesn't work
-  .pipe(closure({compilerPath: 'node_modules/closure-compiler/lib/vendor/compiler.jar', 
-                 fileName: 'dalliance-all.js',
-                 compilerFlags: {
-                    language_in: 'ECMASCRIPT5'
-                 }}))
-  .pipe(gulp.dest('build/'));
+  }).bundle()
+    .pipe(source('js/exports.js'))
+    .pipe(buffer())
+    .pipe(rename('dalliance-all.js'))
+    .pipe(gulp.dest('tmp/'))
+    // .pipe(gif(!isDev, closure()))   // Doesn't work
+    .pipe(closure({
+      compilerPath: 'node_modules/google-closure-compiler/compiler.jar',
+      fileName: 'dalliance-all.js',
+      compilerFlags: {
+        language_in: 'ECMASCRIPT5'
+      }
+    }))
+    .pipe(gulp.dest('build/'));
 });
 
-gulp.task('watch', function() {
+gulp.task('watch', function () {
   gulp.watch('js/*.js', ['default']);
 });
 
